e4704ad8addf31095f35db7de8429525e6330686
keneze.nwangele@gmail.com <you@example.com>
2023-02-02 16:25:43 +0100

    Did lesson 3

---
+++ lesson3/js/script.js
@@ -0,0 +1,6 @@
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+document.getElementById("theYear").textContent = myYear;
+
+
+document.getElementById("lu").textContent = "Last Updated: " + document.lastModified;
