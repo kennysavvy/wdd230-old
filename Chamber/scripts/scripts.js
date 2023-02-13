422b780feb67fb4b04dcd8785da1b86c8b41f893
keneze.nwangele@gmail.com <you@example.com>
2023-02-13 11:57:47 +0100

    o

---
+++ Chamber/scripts/scripts.js
@@ -0,0 +1,23 @@
+
+const datefield = document.querySelector(".date");
+const now = new Date();
+
+const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+let year = document.querySelector("#year");
+const currentYear =  new Date().getFullYear();
+year.textContent= currentYear;
+
+let lastModif = new Date(document.lastModified);
+document.getElementById("modified").innerHTML = lastModif;
+
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburguerBtn").classList.toggle("open");
+
+}
+
+const x = document.getElementById("hamburguerBtn");
+x.onclick = toggleMenu;
